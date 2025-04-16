c2d8adb652f5d1beab0723c2f8b27d83916efe91
mtorres-lechuganegra <marquillo01@gmail.com>
2025-04-15 19:38:20 -0500

    Add name routes

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use LechugaNegra\KeyManager\Http\Controllers\ApiKeyController;
 
-Route::prefix('api/key')->group(function () {
-    Route::post('generate/', [ApiKeyController::class, 'generate']); // Crear nueva API Key
-    Route::delete('revoke/{key}', [ApiKeyController::class, 'revoke']); // Revocar API Key
+Route::prefix('api/key')->name('api.key.')->group(function () {
+    Route::post('generate/', [ApiKeyController::class, 'generate'])->name('me'); // Crear nueva API Key
+    Route::delete('revoke/{key}', [ApiKeyController::class, 'revoke'])->name('me'); // Revocar API Key
 });
 
 Route::middleware(['api.key'])->group(function () {
-    Route::prefix('api/key')->group(function () {
-        Route::post('validate/', [ApiKeyController::class, 'validate']); // Crear nueva API Key
+    Route::prefix('api/key')->name('api.key.')->group(function () {
+        Route::post('validate/', [ApiKeyController::class, 'validate'])->name('validate'); // Crear nueva API Key
     });
 });
