214adbe4bb4e15ec4d5390215c7b572d845d8d2e
mtorres-lechuganegra <marquillo01@gmail.com>
2025-04-29 12:39:19 -0500

    Add auth api

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use LechugaNegra\KeyManager\Http\Controllers\ApiKeyController;
 
-Route::prefix('api/key')->name('api.key.')->group(function () {
-    Route::post('generate/', [ApiKeyController::class, 'generate'])->name('generate'); // Crear nueva API Key
-    Route::delete('revoke/{key}', [ApiKeyController::class, 'revoke'])->name('revoke'); // Revocar API Key
+Route::middleware(['auth:api'])->group(function () {
+    Route::prefix('api/key')->name('api.key.')->group(function () {
+        Route::post('generate/', [ApiKeyController::class, 'generate'])->name('generate'); // Crear nueva API Key
+        Route::delete('revoke/{key}', [ApiKeyController::class, 'revoke'])->name('revoke'); // Revocar API Key
+    });
 });
 
 Route::middleware(['api.key'])->group(function () {
