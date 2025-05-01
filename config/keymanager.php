198a24a09d47f227cd2c6dc41bafa32fb9377ad1
mtorres-lechuganegra <marquillo01@gmail.com>
2025-05-01 13:45:02 -0500

    Add variables in config

--- config/keymanager.php
+++ config/keymanager.php
@@ -11,8 +11,10 @@ return [
     */
     'default_page' => env('KEY_MANAGER_DEFAULT_PAGE', 1),
     'default_size' => env('KEY_MANAGER_DEFAULT_SIZE', 20),
+    'max_size' => env('KEY_MANAGER_MAX_SIZE', 100),
     'default_skip' => env('KEY_MANAGER_DEFAULT_SKIP', 0),
     'default_take' => env('KEY_MANAGER_DEFAULT_TAKE', 20),
+    'max_take' => env('KEY_MANAGER_MAX_TAKE', 100),
 
     /*
     |--------------------------------------------------------------------------
